f29655bc3ff0c9a7fa4191067efb98a7c8d5c636
AdiKachikwu <kachikwuchukwuadinula@gmail.com>
2026-02-18 22:42:54 +0000

    The first Commit from the original codespace including the start of the Encrypt function

--- encrypted_file.py
+++ encrypted_file.py
@@ -1,2 +1,17 @@
-import cryptography
+#First Encryption and Decryption Using the Vigenere Cypher
+key = input("Enter your Key:  ")
+message = input("Enter your message:  ")
+
+
+#Current constraints it returns the message in capitals 
+#Used sites, W3SCHOOLS AND GEEKSFORGEEKS
+
+def Encrypt(key, message):
+    msg = ""
+    Encrypted_message = []
+    key_split = []
+    message_split = []
+    #Adding the key and message into the array 
+    initial_key_split = list(key)
+    initial_message_split = list(message)
 
