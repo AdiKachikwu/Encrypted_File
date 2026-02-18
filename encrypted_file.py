9fc0396c2dbe8c693d9ef7090a9f96f7d811b3d5
AdiKachikwu <kachikwuchukwuadinula@gmail.com>
2026-02-18 22:45:53 +0000

    The completed Encrypt function with two key points: 1) It can only produce capital letters 2) The math is dependent on the ordinal subtraction and adding of A

--- encrypted_file.py
+++ encrypted_file.py
@@ -15,3 +15,39 @@ def Encrypt(key, message):
     initial_key_split = list(key)
     initial_message_split = list(message)
 
+    ##Uppercasing the key and message 
+    for i in range(len(initial_key_split)):
+        key_split.append(initial_key_split[i].upper())
+    
+    for i in range(len(initial_message_split)):
+        message_split.append(initial_message_split[i].upper())
+
+
+    #Function for Vigenere Cypher
+    for i in range(len(message_split)):
+        #Calculation of Vigenere Cypher
+        encrypted_key = ((ord(key_split[i % len(key_split)]) - (ord("A") - 1)) + (ord(message_split[i]) - (ord("A") - 1))) % 26
+        chr_encrypted_key = chr((encrypted_key) + ord("A"))
+        Encrypted_message.append(chr_encrypted_key)
+    
+    #Just so that the outputed message comes in a string instead of a list
+    for i in range(len(Encrypted_message)):
+        msg_initial = str(Encrypted_message[i])
+        new_msg = msg +""+ msg_initial
+        msg = new_msg 
+        
+        
+    print(msg)
+    
+
+    
+    
+
+Encrypt(key, message)    
+    
+
+
+
+
+
+
